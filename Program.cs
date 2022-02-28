088c8dee0053f6f93136b7c7a8441cc4d5fb086d
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-03-01 00:14:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasljedivanjeBiljke
+{
+    class Biljka
+    {
+
+    }
+
+    class Stablo : Biljka
+    {
+        bool opadajuListovi;
+
+        public Stablo(bool opadajuListovi)
+        {
+            this.opadajuListovi = opadajuListovi;
+        }
+
+        public bool OpadajuListovi { get => opadajuListovi; set => opadajuListovi = value; }
+    }
+
+    class Cvijet : Biljka
+    {
+
+    }
+
+    class Bijelogoricno : Stablo
+    {
+
+        public Bijelogoricno(bool opadajuListovi) : base(opadajuListovi)
+        {
+            opadajuListovi = true;
+            OpadajuListovi = opadajuListovi;
+        }
+
+        public virtual string Ispis()
+        {
+            return "Stablu otpadaju listovi. " + OpadajuListovi + ".";
+        }
+    }
+
+    class Crnogoricno : Stablo
+    {
+        public Crnogoricno(bool opadajuListovi) : base(opadajuListovi)
+        {
+            opadajuListovi = false;
+            OpadajuListovi = opadajuListovi;
+        }
+        public virtual string Ispis()
+        {
+
+            return "Stablu otpadaju listovi. " + OpadajuListovi + ".";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bijelogoricno Hrast = new Bijelogoricno(true);
+            Crnogoricno Bor=new Crnogoricno(false);
+
+            Console.WriteLine(Hrast.Ispis());
+            Console.WriteLine(Bor.Ispis());
+
+            Console.ReadKey();
+
+        }
+    }
+}
